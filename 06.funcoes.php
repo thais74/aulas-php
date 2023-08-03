77ca9a21421c57639f54581076eeed143b534a2e
Thais <thaissoares567123@gmail.com>
2023-08-03 09:37:08 -0300

    Update 06.funcoes.php

--- 06.funcoes.php
+++ 06.funcoes.php
@@ -16,7 +16,7 @@ function dadosAutor(){
     echo "<div>";
     echo "<p> Thais da Silva Soares</p>";
     echo "<p> <a href='' > email@aa.com.br";
-    echo "</div>"
+    echo "</div>";
 }
     
 ?>
@@ -41,6 +41,44 @@ function dadosCurso(){
 
 <p>Estamos no curso de <?=dadosCurso()?></p>
 <p><?=dadosCurso()?> é ministrado no Senac Penha</p>
+
+<h2>Função com parâmetros (ou argumentos)</h2>
+<?php
+function soma($valor1, $valor2, $valor3){
+    $total = $valor1 + $valor2 + $valor3;
+    return $total;
+}
+
+?>
+
+<p> Resultado 1:  <?=soma(10, 20, 5)?></p>
+<p> Resultado 2:  <?=soma(23, 84, 1000.75)?></p>
+<?php
+$resultado3 =soma(500, 480, 700);
+?>
+
+<p>Resultado 3: <?=$resultado3?></p>
+
+<?php if($resultado3 > 100) { ?>
+<p>Vinicius foi reprovado</p>
+<?php } ?>
+
+<?php if (soma (2, 3, 10) >= 10){ ?>
+    <p>Leandro reprovado!!</p>
+<?php } 
+
+?>
+
+<?php
+function saudacao ($mensagem, $pessoa = "Fulano(a)"){
+    return "Olá, $mensagem $pessoa!";
+}
+?>
+
+<p><?=saudacao("Bom dia", "Melissa")?></p>
+<p><?=saudacao("Boa noite", "Tanaka")?></p>
+<p><?=saudacao("Boa tarde")?></p>
+
 
 </body>
 </html>
