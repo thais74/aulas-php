f93c0087e492639f1b93339c920f8613e6c75cd2
Thais <thaissoares567123@gmail.com>
2023-08-03 10:08:08 -0300

    final

--- 06.funcoes.php
+++ 06.funcoes.php
@@ -79,6 +79,33 @@ function saudacao ($mensagem, $pessoa = "Fulano(a)"){
 <p><?=saudacao("Boa noite", "Tanaka")?></p>
 <p><?=saudacao("Boa tarde")?></p>
 
+<h2>Indução de tipos de dados</h2>
+<?php
+/* Tipos de dados mais comuns
+string --> caracteres em geral
+int --> números inteiros
+float --> números com casas decimais
+array --> vetor
+*/
+
+/* Indução de tipos deve ser feita nos parâmetros e no retorno da função.
+
+Isso auxiliatá na entrada/saída correta dos dados
+manipulados pela função, além de melhorar as mensagens de erro quando ocorrem problemas. */
+
+
+function verificaNegativo($valor):string{
+    if($valor < 0){
+        return "é negativo";
+        else{
+            return "não é negativo";
+        }
+    }
+}
+?>
+
+<p>Número 10: <?=verificaNegativo(10)?></p>
+<p>Número -10: <?=verificaNegativo(-10)?></p>
 
 </body>
 </html>
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Exemplos e exercícios de PHP
 
-Atividades realizadas entra aulas das UCs 6 (lógica) e 7 (back-end).
+Atividades realizadas entra aulas das UCs 6 (lógica) e 7 (back-end)
