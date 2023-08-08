e2870746850d16bdf0f5d68009023403308ace9a
Thais <thaissoares567123@gmail.com>
2023-08-08 08:46:47 -0300

    update

---
+++ 09-site/contato.php
@@ -0,0 +1,9 @@
+<?php require "includes/cabecalho.php" ?>    
+
+
+<article>
+    <h2>Contato</h2>
+</article>
+
+
+<?php require "includes/rodape.php" ?>
--- 09-site/includes/rodape.php
+++ 09-site/includes/rodape.php
@@ -1,7 +1,18 @@
 </main>
-    
+    <body>
+        
     <footer>
         <p>Site criado para o curso <b>Téc. Informática para Internet</b></p>
     </footer>
+
+<?php
+date_default_timezone_set("America/Sao_Paulo");
+$currentdatetime = date("d-m-y");
+$currentHour = date("H:i");
+?>
+
+<p><?=$currentdatetime?></p>
+<p><?=$currentHour?></p> 
+
 </body>
 </html>
